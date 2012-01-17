8136595b6dce0f54b3c4f61ee3f1098d3facac67
Alexander Malysh <malysh00@gmail.com>
2012-01-17 18:46:43 +0100

    * workaround for the bug when cache_classes=true and model uses serialize attributes

--- lib/active_scaffold/actions/show.rb
+++ lib/active_scaffold/actions/show.rb
@@ -35,6 +35,8 @@ module ActiveScaffold::Actions
     # May be overridden to customize show routine
     def do_show
       @record = find_if_allowed(params[:id], :read)
+      @record.inspect()
+      @record
     end
 
     # The default security delegates to ActiveRecordPermissions.
